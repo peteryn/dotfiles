05b171079b5020921b14d07ef37c94ad5f2989ad
Peter Yuan <peter.yuan18@gmail.com>
2026-04-26 20:13:33 -0400

    Add option to ignore case when searching in nvim

--- .config/nvim/lua/configs.lua
+++ .config/nvim/lua/configs.lua
@@ -23,3 +23,5 @@ vim.cmd('autocmd BufEnter * set formatoptions-=cro')
 vim.cmd('autocmd BufEnter * setlocal formatoptions-=cro')
 
 vim.opt.signcolumn = "yes:1" -- Always show sign column
+
+vim.opt.ignorecase = true -- Ignore case in search
